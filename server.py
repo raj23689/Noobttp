393d285cca5423c026d7762c0a1154838cae9872
raj23689 <92665411+raj23689@users.noreply.github.com>
2024-02-24 22:37:43 +0530

    Added TCP impl.

---
+++ server.py
@@ -0,0 +1,37 @@
+class TCPServer:
+    """Base server class for handling TCP connections.
+    The HTTP server will inherit from this class.
+    """
+
+    def __init__(self, host="127.0.0.1", port=8888):
+        self.host = host
+        self.port = port
+
+    def start(self):
+        """Method for starting the server"""
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((self.host, self.port))
+        s.listen(5)
+
+        print("Listening at", s.getsockname())
+
+        while True:
+            conn, addr = s.accept()
+            print("Connected by", addr)
+
+            # For the sake of this tutorial,
+            # we're reading just the first 1024 bytes sent by the client.
+            data = conn.recv(1024)
+
+            response = self.handle_request(data)
+
+            conn.sendall(response)
+            conn.close()
+
+    def handle_request(self, data):
+        """Handles incoming data and returns a response.
+        Override this in subclass.
+        """
+        return data
